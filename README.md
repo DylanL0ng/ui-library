33178391cb74a4cf56b02cf0a5467e71a8300958
DylanL0ng <dylanhussain08@gmail.com>
2019-08-18 18:20:14 +0100

    commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Boilerplate
+# ui-library
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import Tooltip from './ui/tooltip'
+import Dropdown from './ui/dropdown'
+import Tabs from './ui/tabs'
+import Snackbar from './ui/snackbar'
+
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
+
+// create a dropdown
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach( dropdown => {
+    const instance = new Dropdown(dropdown)
+    instance.init()
+})
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'))
+    tabs.init()
+
+
+// create snackbar
+const snackbar = new Snackbar()
+    snackbar.init();
+    const button = document.querySelector('.snackbar-trigger')
+    button.addEventListener('click', () => {
+        snackbar.show('You Clicked Me!')
+    })
